a8f52885249261a0802f9f09c080d206617acfe1
Will Taylor <will.taylor@mail.utoronto.ca>
2024-01-04 20:37:12 -0700

    Built foundations for rest of R-type instructions. Links to new instructions.svh header.

--- hdl/submods/decoder.sv
+++ hdl/submods/decoder.sv
@@ -4,6 +4,9 @@
 
 //TODO: Implement all RV32I instructions
 //TODO: Add error checking with an error output for unsupported instructions
+
+
+
 module decoder(
     input logic clock,
     input logic reset,
@@ -14,11 +17,9 @@ module decoder(
     output logic alu_op
 );
 
-//TODO: Rest of the opcodes
-typedef enum bit[6:0] {
-    OP      = 7'b0110011,
-    OP_IMM  = 7'b0010011
-} opcode;
+//`include "instructions.svh"
+import OPCODES::*;
+import RTYPE::*;
 
 typedef enum bit {  
     ALU_ADD = 1'b0,
@@ -27,24 +28,46 @@ typedef enum bit {
 
 always_comb begin
     //Default
-    rf_reg1                 = 5'b0;
-    rf_reg2                 = 5'b0;
-    rf_regw                 = 5'b0;
-    alu_op                  = 1'b0;
+    rf_reg1                 = 0;
+    rf_reg2                 = 0;
+    rf_regw                 = 0;
+    alu_op                  = 0;
 
     //Override
     if (reset) begin end //Keep default
     else case (instr[6:0])
         OP: begin
-            rf_reg1         = instr[19:15];
-            rf_reg2         = instr[24:20];
-            rf_regw         = instr[11:7];
-            alu_op          = instr[30] ? ALU_SUB : ALU_ADD;
+            rf_reg1 = instr[19:15];
+            rf_reg2 = instr[24:20];
+            rf_regw = instr[11:7];
+            case (instr[31:25])
+                RTYPE::RA: begin
+                    case (instr[14:12])
+                        RTYPE::ADD: alu_op = ALU_ADD;
+                        RTYPE::SLL: begin end //Keep default
+                        RTYPE::SLT: begin end //Keep default
+                        RTYPE::SLTU: begin end //Keep default
+                        RTYPE::XOR: begin end //Keep default
+                        RTYPE::SRL: begin end //Keep default
+                        RTYPE::OR: begin end //Keep default
+                        RTYPE::AND: begin end //Keep default
+                        default: begin end //Error signal goes here in future.
+                    endcase
+                end
+                RTYPE::RB: begin
+                    case (instr[14:12])
+                        RTYPE::SUB: alu_op = ALU_SUB;
+                        RTYPE::SRA: begin end //Keep default
+                        default: begin end //Error signal goes here in future.
+                    endcase
+                end
+                default: begin end //Error signal goes here in future.
+            endcase
         end
         OP_IMM: begin
             //TODO: Implement
         end
-        default: begin end //Keep default. Error signal goes here in future.
+        default: begin end //Error signal goes here in future.
     endcase
 end
 
